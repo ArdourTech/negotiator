36c13eef4c1107a3d96a0c4db18f03f44095e53d
Alexander Scott <xander@axrs.io>
2021-01-09 22:37:41 +1000

    Init Project

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1 (2021-01-09)
+
+* Library extracted from Monolith
---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject tech.ardour/negotiator "0.0.1"
+  :description "Ardour Tech Content Negotiation Library"
+  :url "https://github.com/ArdourTech/negotiator"
+  :license {:name         "Eclipse Public License - v 1.0"
+            :url          "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments     "same as Clojure"}
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [metosin/jsonista "0.3.0"]]
+  :profiles {:dev  {:dependencies [[org.clojure/clojurescript "1.10.597" :scope "provided"]]}
+             :test {:dependencies [[org.clojure/data.json "1.0.0"]
+                                   [lambdaisland/kaocha "1.0.732"]]}}
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                    :sign-releases false}]]
+  :aliases {"test" ["with-profile" "+test" "run" "-m" "kaocha.runner"]})
